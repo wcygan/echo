02ca7b0845668db416cc0cac0c4654be3384816a
Will Cygan <wcygan.io@gmail.com>
2023-03-30 23:47:56 -0700

    nits

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,15 +4,18 @@ use connection::Connection;
 #[tokio::main]
 async fn main() -> Result<()> {
     let listener = tokio::net::TcpListener::bind("127.0.0.1:7272").await?;
+    let addr = listener.local_addr()?;
+    println!("Listening on: {}", addr);
 
     loop {
-        let (socket, _) = listener.accept().await?;
+        let (socket, addr) = listener.accept().await?;
         tokio::spawn(async move {
+            println!("Peer connected from {}", addr);
             let mut conn = Connection::new(socket);
 
             loop {
                 let s = conn.read::<String>().await.unwrap().unwrap();
-                println!("{:?}", s);
+                println!("{}: {:?}", addr, s);
                 conn.write::<String>(&s).await.unwrap();
             }
         });
